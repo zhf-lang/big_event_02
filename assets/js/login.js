cf75f09b6b8bc0883fa1888340cdb0ca88bf3a29
zhu310 <2473761811@qq.com>
2021-03-13 20:26:01 +0800

    登录注册完成

---
+++ assets/js/login.js
@@ -0,0 +1,65 @@
+$(function () {
+    $('.link_login').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide();
+    })
+    $('.link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    })
+
+    let form = layui.form;
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function (value, item) {
+            if (value != $('.reg-box [name=password]').val()) {
+                return '两次密码输入不一致';
+            }
+        }
+    })
+
+    let layer = layui.layer;
+    $('#reg-form').on('submit', function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: 'http://api-breakingnews-web.itheima.net/api/reguser',
+            type: 'post',
+            data: {
+                username: $('.reg-box [name=username]').val(),
+                password: $('.reg-box [name=password]').val()
+            },
+            success: (res) => {
+                console.log(res);
+                if (res.status != 0) {
+                    layer.msg(res.message, { icon: 5 });
+                }
+                layer.msg(res.message, { icon: 6 });
+                $('#reg-form')[0].reset();
+                $('.link_login').click();
+            }
+        })
+    })
+
+    $('#login-form').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'http://api-breakingnews-web.itheima.net/api/login',
+            type: 'post',
+            data: $(this).serialize(),
+            success: (res) => {
+                console.log(res);
+                
+                if (res.status != 0) {
+                   return layer.msg(res.message, { icon: 5 });
+                }
+                layer.msg(res.message, { icon: 6 });
+                localStorage.setItem('token',res.token)
+                location.href = "/index.html";
+            }
+        })
+    })
+})
